d061091523c09f7ca23e8c3e62ddc360198c2278
Leandro Humberto Vieira <leandrohmvieira@gmail.com>
2018-09-10 10:44:19 -0300

    fixed a typo

--- connector.py
+++ connector.py
@@ -93,30 +93,3 @@ metadata = regex_metadata.findall(section_script)
 metadata
 
 #writing database csv file
-
-output = open("output/database_sources.csv","w",encoding='utf-8')
-
-#Write the database Host
-output.write("+++ Host - begin +++\n")
-output.write("Name,Description\n")
-output.write(metadata[0]+",\n")
-output.write("+++ Host - end +++\n\n")
-
-#write database definitions
-output.write("+++ Database - begin +++\n")
-output.write("Name,Host,Description,Vendor,Version,Instance,Location,DBMS\n")
-output.write(metadata[1]+","+metadata[0]+",\n")
-
-## TODO: build a pandas dataframe with database definitions, or any another tabular writing to the file
-output.write("+++ Database - end +++")
-
-
-#write schema definitions
-output.write("+++ Schema - begin +++\n")
-output.write("Name,Host,Database,Description\n")
-## TODO: build a pandas dataframe with schema definitions, or any another tabular writing to the file
-
-output.write("+++ Schema - end +++")
-output.close()
-
-## TODO: evaluate if its possible to bring table level metadata to the dance floor
