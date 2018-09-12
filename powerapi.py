cd0654ae092a573f491f5e55f9fdf8c1d364e58c
Leandro Humberto Vieira <leandrohmvieira@gmail.com>
2018-09-12 11:55:17 -0300

    added a pbiServer class, that wil hold all access methods to database

--- powerapi.py
+++ powerapi.py
@@ -1,21 +1,41 @@
-#this file have all communication methods between this application and pbi Report Server
+#library to manage all IO operations with the Power Bi Report Server
+import pyodbc
+import os
+from dotenv import load_dotenv, find_dotenv
+import pandas as pd
+
+class PbiServer:
+
+    server = None
+    database = None
+    username = None
+    password = None
+    connection = None
+
+    def __init__(self):
+        load_dotenv(find_dotenv())
+        self.server = os.getenv("SERVER")
+        self.database = os.getenv("DATABASE")
+        self.username = os.getenv("USER")
+        self.password = os.getenv("PASSWORD")
+        self.connection = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+ self.password)
 
 
-def download_reports(reports):
+    def download_all_reports(self,reports_directory="input/reports/"):
 
-    for report in reports.itertuples():
-        #get report id and name
-        reportid = report.itemid
-        report_name = report.name
+        all_reports_query = open('select_all_reports.sql', 'r').read()
+        report_content_query = open('select_report_content.sql', 'r').read()
+        reports = pd.read_sql_query(all_reports_query, self.connection)
 
-        print("Downloading {}".format(report.name))
-        print("{}% completed".format(int(100 * float(report.Index+1)/float(reports.shape[0]))))
-        os.sys.stdout.write('\r')
-        #download report into reports Folder
-        report_content = pd.read_sql_query(report_content_query,cnxn,params=[reportid])
-        input_filename = "input/reports/"+report_name+".pbix"
-        with open(input_filename, "wb") as pbix_file:
-            pbix_file.write(report_content['BinaryContent'][0])
-        pbix_file.close()
-        del pbix_file
-    return 1
+        for report in reports.itertuples():
+            print("Downloading {}".format(report.name))
+            print("{}% completed".format(int(100 * float(report.Index+1)/float(reports.shape[0]))))
+            os.sys.stdout.write('\r')
+            #download report into reports Folder
+            report_content = pd.read_sql_query(report_content_query,self.connection,params=[report.itemid])
+            input_filename = reports_directory+report.name+".pbix"
+            with open(input_filename, "wb") as pbix_file:
+                pbix_file.write(report_content['BinaryContent'][0])
+            pbix_file.close()
+            del pbix_file
+        return reports
