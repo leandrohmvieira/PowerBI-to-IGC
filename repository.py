2d04f39e1b2492af8ff240944ae616fb82c15015
Leandro Humberto Vieira <leandrohmvieira@gmail.com>
2018-09-13 11:33:42 -0300

    added clearing methods to repository

--- repository.py
+++ repository.py
@@ -1,12 +1,31 @@
-class repository:
+import shutil
+import glob
 
-    path = None
+class Repository:
+
+    base = None
+    reports = None
+    metadata = None
+    temp = None
 
     def __init__(self,path='input/'):
-        self.path = path
+        self.base = path
+        self.reports = path+'reports/'
+        self.metadata = path+'metadata/'
+        self.temp = path+'temp/'
 
-    def clear_temp():
-        files = glob.glob('input/temp/**')
-        files
+    def clear_temp(self):
+        files = glob.glob(self.temp+'**',recursive=True)
+        for f in files:
+            shutil.rmtree(f,ignore_errors=True)
+        
+
+    def clear_reports(self):
+        files = glob.glob(self.reports+'**',recursive=True)
+        for f in files:
+            shutil.rmtree(f,ignore_errors=True)
+
+    def clear_metadata(self):
+        files = glob.glob(self.metadata+'**',recursive=True)
         for f in files:
             shutil.rmtree(f,ignore_errors=True)
