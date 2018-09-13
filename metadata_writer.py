684cb8ed6efb8c8494fb8dc4317762555f68c7f2
Leandro Humberto Vieira <leandrohmvieira@gmail.com>
2018-09-13 11:39:10 -0300

    renamed database_writer, this will have to be worked on later

--- metadata_writer.py
+++ metadata_writer.py
@@ -1,4 +1,24 @@
 
+# TODO: organize this sh*t
+
+#writing CSV compatible output file
+output = open("output/bi_reports.csv","w",encoding='utf-8')
+
+#Write the PowerBI Host
+output.write("+++ BI Server - begin +++\n")
+output.write("Name,Description\n")
+output.write('server'+",\n")
+output.write("+++ BI Server - end +++\n\n")
+
+#defining a range of columns to fill the file
+report_columns = ['name','server','folder','description']
+
+#Write PowerBI Reports
+output.write("+++ BI Report - begin +++\n")
+output.write("Name,Server,Folder,Description\n")
+reports[report_columns].to_csv(output,header=False,index=False) #using reindex due to a future warning
+output.write("+++ BI Report - end +++")
+output.close()
 
 
 
