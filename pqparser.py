a941a5b9ec4a3889b69320cd5e3879acefcb9d70
Leandro Humberto Vieira <leandrohmvieira@gmail.com>
2018-09-14 12:00:43 -0300

    filename was conflicting with python internal parser

--- pqparser.py
+++ pqparser.py
@@ -7,3 +7,8 @@ import re
 
 get_variables = re.compile('shared (\w+) = \"([\w.:]+)\"') # 2 groups, first group is variable name and the second is variable value
 get_sources = re.compile('DB2\.Database\W*\((\w+)\W+(\w+)[\s\S]+?Query=\"([\s\S]+?)\"') # 3 groups, host variable, database variable, and Query executed
+
+def has_db2_sources(itemid):
+    file = open('input/metadata/'+itemid+'.m','r',encoding='utf-8').read()
+    db2_sources = get_sources.findall(file)
+    return len(db2_sources) > 0
