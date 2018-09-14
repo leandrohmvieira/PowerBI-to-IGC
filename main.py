fb9be77d1472e07126d93035bbce776469971199
Leandro Humberto Vieira <leandrohmvieira@gmail.com>
2018-09-14 11:41:23 -0300

    moved .m script extraction process to repository

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 #import my libs
 import powerapi
 from repository import Repository
-
-#import 3rd party libs
-import glob
-import zipfile
-import subprocess
-import shutil
+import parser
 
 #get environment variables from .env file
 import os
@@ -18,26 +13,4 @@ pbi = powerapi.PbiServer()
 repo = Repository()
 
 #pbi.download_all_reports(repo)
-
-for file in glob.glob(repo.reports+'*'):
-
-    input_filename = file
-    zip_ref = zipfile.ZipFile(input_filename, 'r')
-    zip_ref.extractall('input/temp')
-    zip_ref.close()
-    #Unzipping the DataMashup file (yeah, PowerBI is all about zipped files)
-    mashup_file = 'input/temp/DataMashup'
-    sevenzip_path = os.getenv("7ZIP")
-    source = mashup_file
-    directory = '-oinput/temp' ## TODO: fix this -o thing
-
-    #build and execute a 7zip call to unzip the DataMashup file
-    cmd = [sevenzip_path, 'e', source , directory,'-aoa']
-    sp = subprocess.Popen(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-    sp.wait()
-    #to get the command output, use sp.communicate()
-    #sp.communicate()
-    ## TODO: Find a way to remove this 3rd party process dependence
-    #get Section1.m file and move it to another folder
-    shutil.move('input/temp/Section1.m', 'input/metadata/'+input_filename.split('\\')[1].split('.')[0]+'.m')
-    repo.clear_temp()
+#repo.extract_pbi_queries()
--- repository.py
+++ repository.py
@@ -1,5 +1,8 @@
-import shutil
+import os
 import glob
+import zipfile
+import subprocess
+import shutil
 
 class Repository:
 
@@ -18,7 +21,7 @@ class Repository:
         files = glob.glob(self.temp+'**',recursive=True)
         for f in files:
             shutil.rmtree(f,ignore_errors=True)
-        
+
 
     def clear_reports(self):
         files = glob.glob(self.reports+'**',recursive=True)
@@ -29,3 +32,27 @@ class Repository:
         files = glob.glob(self.metadata+'**',recursive=True)
         for f in files:
             shutil.rmtree(f,ignore_errors=True)
+
+    def extract_pbi_queries(self):
+
+        for file in glob.glob(self.reports+'*'):
+
+            input_filename = file
+            zip_ref = zipfile.ZipFile(input_filename, 'r')
+            zip_ref.extractall(self.temp)
+            zip_ref.close()
+            #Unzipping the DataMashup file (yeah, PowerBI is all about zipped files)
+            sevenzip_path = os.getenv("7ZIP")
+            source = self.temp+'DataMashup'
+            directory = '-o'+self.temp
+
+            #build and execute a 7zip call to unzip the DataMashup file
+            cmd = [sevenzip_path, 'e', source , directory,'-aoa']
+            sp = subprocess.Popen(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+            sp.wait()
+            #to get the command output, use sp.communicate()
+            #sp.communicate()
+            ## TODO: Find a way to remove this 3rd party process dependence
+            #get Section1.m file and move it to another folder
+            shutil.move(self.temp+'Section1.m', self.metadata+input_filename.split('\\')[1].split('.')[0]+'.m')
+            self.clear_temp()
