e1f2ba57da8e3f845d44822219e63611bd0cd8fc
Leandro Humberto Vieira <leandrohmvieira@gmail.com>
2018-09-17 12:04:17 -0300

    new method get_metadata, which returns a dict with host, db and query

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #import my libs
 import powerapi
 from repository import Repository
-import parser
+import pqparser as pqp #pq stands for Power Query, dont get me wrong
+import numpy as np
 
 #get environment variables from .env file
 import os
@@ -14,3 +15,22 @@ repo = Repository()
 
 #pbi.download_all_reports(repo)
 #repo.extract_pbi_queries()
+
+reports = pbi.get_report_list()
+
+import numpy as np
+
+#function applying methods:
+reports['db2'] = np.vectorize(pqp.has_db2_sources)(reports['itemid'])
+#reports['db2'] = reports['db2'].map(pqp.has_db2_sources)
+
+db2_reports = reports[reports.db2].reset_index()
+
+db2_reports['itemid'][0]
+
+meta = pqp.get_metadata(db2_reports['itemid'][0])
+
+meta[0].get('query')
+# TODO: time to parse this query
+
+meta = meta[0]
--- pqparser.py
+++ pqparser.py
@@ -9,6 +9,23 @@ get_variables = re.compile('shared (\w+) = \"([\w.:]+)\"') # 2 groups, first gro
 get_sources = re.compile('DB2\.Database\W*\((\w+)\W+(\w+)[\s\S]+?Query=\"([\s\S]+?)\"') # 3 groups, host variable, database variable, and Query executed
 
 def has_db2_sources(itemid):
-    file = open('input/metadata/'+itemid+'.m','r',encoding='utf-8').read()
-    db2_sources = get_sources.findall(file)
-    return len(db2_sources) > 0
+    try:
+        file = open('input/metadata/'+itemid+'.m','r',encoding='utf-8').read()
+        db2_sources = get_sources.findall(file)
+        return len(db2_sources) > 0
+    except IOError as e:
+        return None
+
+def get_metadata(itemid):
+    if has_db2_sources(itemid):
+        file = open('input/metadata/'+itemid+'.m','r',encoding='utf-8').read()
+        variables = dict(get_variables.findall(file))
+        db2_sources = get_sources.findall(file)
+        fields = ['host', 'database', 'query']
+        metadata = [dict(zip(fields, source)) for source in db2_sources]
+        for vars in metadata:
+            vars.update({'host':variables.get(vars.get('host'))})
+            vars.update({'database':variables.get(vars.get('database'))})
+        return metadata
+    else:
+        return None
