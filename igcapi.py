6cfbe9bb8914aa548377073dce14dcc7c838b9a3
Leandro Humberto Vieira <leandrohmvieira@gmail.com>
2018-09-19 11:56:01 -0300

    igc api become a class and was added to main

--- igcapi.py
+++ igcapi.py
@@ -1,16 +1,38 @@
+"""
+Python Wrapper around IBM Information Governance Catalog restful service
+"""
+import os
 import requests
-from requests.auth import HTTPProxyAuth
 
-# request demo
-# r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-# r.status_code
-# r.headers['content-type']
-# r.encoding
-# r.text
-# r.json()
-#proxy auth https://stackoverflow.com/questions/13506455/how-to-pass-proxy-authentication-requires-digest-auth-by-using-python-requests
+class igc:
 
-r = requests.get('https://glossario.intranet.bb.com.br:9443/ibm/iis/igc-rest/v1/metadata', proxies=proxies,auth=auth )
+    session = None
+    base_url = 'https://glossario.intranet.bb.com.br:9443/ibm/iis/igc-rest/v1/'
 
+    def __init__(self):
+        s = requests.Session()
+        s.proxies = {"http": os.getenv("PROXY_STRING") , "https": os.getenv("PROXY_STRING")}
+        s.auth = (os.getenv("IGC_USER"),os.getenv("IGC_PASSWORD"))
+        s.verify = False
+        s.trust_env = False
+        self.session = s
+    def get_terms(self):
+        types = 'term'
+        payload = {'types':types}
+        r = self.session.get(self.base_url+'search/',params = payload)
+        # verify=False
+        return r.text
 
-#https://glossario.intranet.bb.com.br:9443/ibm/iis/igc-rest/v1/search/?types=term&text=abatimento%20negocial&search-properties=name
+# types = 'term'
+# text = 'abatimento'
+# search_properties = 'name'
+#
+#
+# payload = {'types':types}
+# r = self.session.get('https://glossario.intranet.bb.com.br:9443/ibm/iis/igc-rest/v1/search/',params = payload,verify=False)
+# print(r.text)
+# resultset = r.json()
+# print(resultset)
+# description = resultset.get('short_description')
+# print(description)
+# resultset = dict(r.text)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import powerapi
 from repository import Repository
 import pqparser as pqp #pq stands for Power Query, dont get me wrong
 import numpy as np
+from igcapi import igc
 
 #get environment variables from .env file
 import os
@@ -12,11 +13,16 @@ load_dotenv(find_dotenv())
 #create a Power Bi Server connection and a local repository of files
 pbi = powerapi.PbiServer()
 repo = Repository()
+igc = igc()
+
+igc.get_terms()
 
 #pbi.download_all_reports(repo)
 #repo.extract_pbi_queries()
 
 reports = pbi.get_report_list()
+
+reports
 
 import numpy as np
 
@@ -30,7 +36,10 @@ db2_reports['itemid'][0]
 
 meta = pqp.get_metadata(db2_reports['itemid'][0])
 
-meta[0].get('query')
-# TODO: time to parse this query
+meta
 
-meta = meta[0]
+query = meta[0].get('query')
+
+query_meta = pqp.get_query_from_tables.findall(query)
+
+query_meta
