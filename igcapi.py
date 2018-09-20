cb0e91cdd43c5aaeb78e838d93e08f14ed11952b
Leandro Humberto Vieira <leandrohmvieira@gmail.com>
2018-09-20 11:40:33 -0300

    working on bundle verification creation and insertion methods

--- igcapi.py
+++ igcapi.py
@@ -7,7 +7,7 @@ import requests
 class igc:
 
     session = None
-    base_url = 'https://glossario.intranet.bb.com.br:9443/ibm/iis/igc-rest/v1/'
+    base_url = None
 
     def __init__(self):
         s = requests.Session()
@@ -16,12 +16,31 @@ class igc:
         s.verify = False
         s.trust_env = False
         self.session = s
+        self.base_url = os.getenv("IGC_BASE_URL")
+
     def get_terms(self):
         types = 'term'
         payload = {'types':types}
         r = self.session.get(self.base_url+'search/',params = payload)
-        # verify=False
         return r.text
+
+    def check_bundle(self):
+        # TODO: verify if our bundle exists on igc
+        r = self.session.get(self.base_url+'bundles/')
+        return 'PowerBI' in r.text
+
+    def register_bundle(self,repository):
+        # TODO: post zipped bundle onto igc
+        file={'file': open(repository.bundle+'bundle.zip','rb')}
+        return self.session.post(self.base_url+'bundles/',params = file)
+
+    def insert_asset(self,asset):
+        # TODO: well, dont know how to do it yet, open to suggestions
+        r = self.session.post(self.base_url+'bundles/assets/',params = payload)
+        return r.text
+
+
+
 
 # types = 'term'
 # text = 'abatimento'
