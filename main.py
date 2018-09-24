86ca6a79fe97b6fe80101d9f75ee138cf4e02b02
Leandro Humberto Vieira <leandrohmvieira@gmail.com>
2018-09-24 12:01:01 -0300

    Reestructured main.py and added some guidelines

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+"""
+Script which contains the main logic of the app, putting all together
+
+The process consists of the following steps:
+
+1 - Drop the bundle if it exists and create it again (for now, in behalf of simplicity, so we will not have to calculate deltas)
+
+2 - Download Power Bi reports from the Power BI Report Server and extract their M scripts
+
+3 - Parse the M scripts and get the metadata ready for loading on IGC(which is XML oriented)
+
+4 - generate the XML from metadata and upload it to IGC
+
+"""
+
 #import my libs
 import powerapi
 from repository import Repository
@@ -10,15 +25,28 @@ import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
-#create a Power Bi Server connection and a local repository of files
+#Create objects to manipulate the PbiServer, local repository and IGC rest api
 pbi = powerapi.PbiServer()
 repo = Repository()
 igc = igc()
 
-igc.get_terms()
 
+igc.internal_id
+
+# Step 1: truncate all objects (temporary)
+r = igc.delete_bundle()
+
+# Step 2: Register bundle
+result = igc.register_bundle(repo)
+
+# Step 3: Download Power BI db2_reports
 #pbi.download_all_reports(repo)
+
+# Step 4: Extract the M scripts of all reports
 #repo.extract_pbi_queries()
+
+# Step 5: Generate a pandas dataframe for hosts, folders, reports and schemas
+
 
 reports = pbi.get_report_list()
 
