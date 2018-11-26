7c41f61e86592b3b3044aae3483ec12ae78a1192
Leandro Humberto Vieira <leandrohmvieira@gmail.com>
2018-11-26 11:43:11 -0200

    now pqparser can get all columns from aliased tables in a query

--- pqparser.py
+++ pqparser.py
@@ -35,7 +35,7 @@ def get_metadata(itemid):
     else:
         return None
 
-def tables_in_query(sql_str):
+def parse_query(sql_str):
 
     # remove the /* */ comments
     q = re.sub(r"/\*[^*]*\*+(?:[^*/][^*]*\*+)*/", "", sql_str)
@@ -46,26 +46,43 @@ def tables_in_query(sql_str):
     # remove trailing -- and # comments
     q = " ".join([re.split("--|#", line)[0] for line in lines])
 
-    # split on blanks, parens and semicolons
-    tokens = re.split(r"[\s)(;]+", q)
+    # split on blanks, parens, semicolons, commas and operators
+    tokens = re.split(r"[\s)(;,=<>]+", q)
 
     # scan the tokens. if we see a FROM or JOIN, we set the get_next
-    # flag, and grab the next one (unless it's SELECT).
-
-    result = set()
+    # flag, and grab the next two tokens
+    tables = []
     get_next = False
     get_alias = False
     for tok in tokens:
         if get_alias:
-            result.add((table,tok))
+            tables.append((table,tok))
             get_alias = False
         elif get_next:
             if tok.lower() not in ["", "select"]:
-                #result.add(tok)
                 table = tok
                 get_alias = True
-            get_next = False
-        get_next = tok.lower() in ["from", "join"]
+                get_next = False
+            else:
+                get_next = False
+        else:
+            get_next = tok.lower() in ["from", "join"]
+    #now with the tables and aliases from the query, we will get the aliased columns from the Query
+    columns = set()
+    for table in tables:
+        tablename = table[0]
+        alias = table[1]
+        # TODO: Get tables from nested queries as well
+        if '.' in tablename:
+            for tok in tokens:
+                if alias+"." in tok:
+                    columns.add((tablename,alias,tok.split(".")[1]))
+                else:
+                    continue
+        else:
+            continue
 
-    return result
+    #Transform tables into a table set, removing dynamic tables
+    tables = set([i[0] for i in tables if '.' in i[0]])
+    return columns,tables,tokens
 # FIXME: the method above doesn't work when the last token is a unaliased table
